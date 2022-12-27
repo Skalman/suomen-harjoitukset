ec0f39d33dfb1c4258989e9bc1ff207d56dff5e2
Dan Wolff <dan.d.wolff@gmail.com>
2022-12-27 18:57:33 +0100

    Format with Prettier

--- js/app.js
+++ js/app.js
@@ -1,178 +1,197 @@
 'use strict';
 
-$( function () {
+$(function () {
+  // The overall view
+  // ----------------
+  $('#init button').on('click', function () {
+    var $this = $(this);
+    var form = {
+      id: $this.data('id'),
+      text: $this.data('text')
+    };
+    console.log('Start', form.id);
 
-	// The overall view
-	// ----------------
-	$( '#init button' ).on( 'click', function () {
-		var $this = $(this);
-		var form = {
-			id: $this.data( 'id' ),
-			text: $this.data( 'text' )
-		};
-		console.log( 'Start', form.id );
+    switch_screen('exercises');
 
-		switch_screen( 'exercises' );
+    get_words()
+      .then(function (words) {
+        return words_to_exercise_list(words, form);
+      })
+      .then(create_app)
+      .then(function (stats) {
+        console.log('Exercise done');
+      })
+      .fail(function (err) {
+        console.error('Caught error:', err);
+      })
+      .then(function () {
+        switch_screen('init');
+        $this.focus();
+      });
+  });
 
-		get_words()
-		.then(function ( words ) {
-			return words_to_exercise_list( words, form );
-		}).then( create_app )
-		.then( function ( stats ) {
-			console.log( 'Exercise done' );
-		}).fail( function ( err ) {
-			console.error( 'Caught error:', err );
-		}).then( function() {
-			switch_screen( 'init' );
-			$this.focus();
-		});
-	});
+  // Message customization
+  // ---------------------
+  App_view.i18n.messages(
+    'fi',
+    'correct_answer_text',
+    '{{PLURAL:$1|Oikea vastaus|Oikeat vastaukset}} Wikisanakirjan mukaan:'
+  );
 
-	// Message customization
-	// ---------------------
-	App_view.i18n.messages( 'fi',
-		'correct_answer_text',
-		'{{PLURAL:$1|Oikea vastaus|Oikeat vastaukset}} Wikisanakirjan mukaan:' );
+  // Get words
+  // ---------
+  // Retrieve a list of words
+  function get_words() {
+    return Q.fcall($.ajax, {
+      url: 'resources/words.json',
+      dataType: 'json'
+    }).then(function (data) {
+      var words = _.map(data.words, _.identity);
+      words = words.concat.apply([], words);
+      words = _.shuffle(words);
 
-	// Get words
-	// ---------
-	// Retrieve a list of words
-	function get_words() {
-		return Q.fcall( $.ajax, {
-			url: 'resources/words.json',
-			dataType: 'json'
-		}).then( function ( data ) {
-			var words = _.map( data.words, _.identity );
-			words = words.concat.apply( [], words );
-			words = _.shuffle( words );
+      return words;
+    });
+  }
 
-			return words;
-		});
-	}
+  // Convert a words to exercises
+  // ----------------------------
+  // Takes a list of words and a form, and returns an Exercise list
+  function words_to_exercise_list(words, form) {
+    function retrieve_answer(exercise) {
+      // and when we got the HTML from Wiktionary
+      var wiki_base = 'https://en.wiktionary.org/w/';
 
-	// Convert a words to exercises
-	// ----------------------------
-	// Takes a list of words and a form, and returns an Exercise list
-	function words_to_exercise_list( words, form ) {
-		function retrieve_answer( exercise ) {
-			// and when we got the HTML from Wiktionary
-			var wiki_base = 'https://en.wiktionary.org/w/';
+      return mediawiki_get_page_html(wiki_base, exercise.get('question')).then(
+        function got_html(html) {
+          var a = get_form(form.id, html);
+          console.log(
+            'Retrieved ' +
+              (a.length === 1 ? 'answer' : a.length + ' answers') +
+              ' from Wiktionary'
+          );
+          return a;
+        }
+      );
+    }
+    return new Exercise_list({
+      instructions: 'Kirjoita sanan ' + form.text,
+      exercises: _.map(words, function (word) {
+        return {
+          question: word,
+          answer: retrieve_answer
+        };
+      })
+    });
+  }
 
-			return mediawiki_get_page_html( wiki_base, exercise.get('question') )
-			.then( function got_html( html ) {
-				var a = get_form( form.id, html );
-				console.log( 'Retrieved ' + (a.length===1 ? 'answer' : a.length+' answers') + ' from Wiktionary' );
-				return a;
-			});
-		}
-		return new Exercise_list({
-			instructions: 'Kirjoita sanan ' + form.text,
-			exercises: _.map( words, function ( word ) {
-				return {
-					question: word,
-					answer: retrieve_answer
-				};
-			})
-		});
-	}
+  // Create the app
+  // --------------
+  function create_app(exercise_list) {
+    var deferred = Q.defer();
 
-	// Create the app
-	// --------------
-	function create_app( exercise_list ) {
-		var deferred = Q.defer();
+    var app = new App_view({
+      exercises: exercise_list,
+      lang: 'fi',
+      elem: $('#exercises')
+    }).on('complete', deferred.resolve);
 
-		var app = new App_view({
-			exercises: exercise_list,
-			lang: 'fi',
-			elem: $('#exercises')
-		}).on( 'complete', deferred.resolve );
+    return deferred.promise;
+  }
 
-		return deferred.promise;
-	}
+  // Get the given form from HTML
+  // ----------------------------
+  // The given HTML should be an English Wiktionary page containing a Finnish section. The form in question, will then be extracted from the inflection table.
+  function get_form(form, html) {
+    var kase, number;
+    if (/^plural\-/.test(form)) {
+      kase = form.substr('plural-'.length);
+      number = 'plural';
+    } else if (/^singular\-/.test(form)) {
+      kase = form.substr('singular-'.length);
+      number = 'singular';
+    }
 
-	// Get the given form from HTML
-	// ----------------------------
-	// The given HTML should be an English Wiktionary page containing a Finnish section. The form in question, will then be extracted from the inflection table.
-	function get_form( form, html ) {
-		var kase, number;
-		if ( /^plural\-/.test(form) ) {
-			kase = form.substr( 'plural-'.length );
-			number = 'plural';
-		} else if ( /^singular\-/.test(form) ) {
-			kase = form.substr( 'singular-'.length );
-			number = 'singular';
-		}
+    html = get_finnish_section(html);
+    var $cell = $($.parseHTML('<div>' + html + '</div>')[0])
+      // find the <tr> containing partitive
+      .find('table.inflection-table tr')
+      .filter(function () {
+        return $(this).find('th').first().text() === kase;
+      })
 
-		html = get_finnish_section( html );
-		var $cell = $($.parseHTML( '<div>' + html + '</div>' )[0])
+      // get the right cell: left for singular, right for plural
+      .find(number === 'singular' ? 'th + td' : 'th + td + td');
 
-			// find the <tr> containing partitive
-			.find( 'table.inflection-table tr' )
-			.filter( function () {
-				return $( this ).find( 'th' ).first().text() === kase;
-			})
+    var variants = _.uniq(
+      $cell
+        .find('a')
+        .map(function () {
+          return $(this).text();
+        })
+        .toArray()
+    );
 
-			// get the right cell: left for singular, right for plural
-			.find( number === 'singular' ? 'th + td' : 'th + td + td' );
+    return variants;
+  }
 
-		var variants = _.uniq( $cell.find( 'a' ).map( function () {
-			return $( this ).text();
-		}).toArray() );
+  // Retrieve the Finnish section
+  // ----------------------------
+  function get_finnish_section(html) {
+    var start, end;
+    start = /<span class="mw-headline"[^>]+>Finnish<\/span>[^\n]*?<\/h2>/.exec(
+      html
+    );
+    if (start) {
+      html = html.substr(start.index + start[0].length);
+      end = /<h2/.exec(html);
+      if (end) {
+        html = html.substr(0, end.index);
+      }
+      return html;
+    } else {
+      console.error('Could not extract the Finnish section from HTML');
+      return null;
+    }
+  }
 
-		return variants;
-	}
+  // Access a page from a wiki
+  // -------------------------
+  function mediawiki_get_page_html(wiki_base, page) {
+    return Q.fcall($.ajax, {
+      url:
+        wiki_base +
+        'api.php?format=json' +
+        '&action=parse' +
+        '&page=' +
+        encodeURIComponent(page),
 
-	// Retrieve the Finnish section
-	// ----------------------------
-	function get_finnish_section( html ) {
-		var start, end;
-		start = /<span class="mw-headline"[^>]+>Finnish<\/span>[^\n]*?<\/h2>/.exec( html );
-		if ( start ) {
-			html = html.substr( start.index + start[0].length );
-			end = /<h2/.exec( html );
-			if ( end ) {
-				html = html.substr( 0, end.index );
-			}
-			return html;
-		} else {
-			console.error( 'Could not extract the Finnish section from HTML' );
-			return null;
-		}
-	}
+      // avoid additional parameter _=TIMESTAMP and caching doesn't matter
+      cache: true,
 
-	// Access a page from a wiki
-	// -------------------------
-	function mediawiki_get_page_html( wiki_base, page ) {
-		return Q.fcall( $.ajax, {
-			url: wiki_base + 'api.php?format=json' +
-				'&action=parse' +
-				'&page=' + encodeURIComponent( page ),
+      dataType: 'jsonp'
+    }).then(function (data) {
+      if (data.error) {
+        throw data.error;
+      } else {
+        return data.parse.text['*'];
+      }
+    });
+  }
 
-			// avoid additional parameter _=TIMESTAMP and caching doesn't matter
-			cache: true,
+  // Switch between #init and #exercises
+  // -----------------------------------
+  function switch_screen(to_id) {
+    $('.screen.active').removeClass('active');
+    $('#' + to_id).addClass('active');
+  }
 
-			dataType: 'jsonp'
-		}).then( function ( data ) {
-			if ( data.error ) {
-				throw data.error;
-			} else {
-				return data.parse.text['*'];
-			}
-		});
-	}
+  // Other
+  // -----
 
-
-	// Switch between #init and #exercises
-	// -----------------------------------
-	function switch_screen( to_id ) {
-		$( '.screen.active' ).removeClass( 'active' );
-		$( '#' + to_id ).addClass( 'active' );
-	}
-
-	// Other
-	// -----
-
-	// If the console is not defined, just define a NOOP
-	var console = window.console || { log: function () {}, error: function () {} };
-
+  // If the console is not defined, just define a NOOP
+  var console = window.console || {
+    log: function () {},
+    error: function () {}
+  };
 });
